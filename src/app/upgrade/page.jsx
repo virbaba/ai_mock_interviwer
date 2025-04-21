a127a28cae9ce4c75ac0804dc855f50495b7ac0f
virbaba <virendersinghdev1@gmail.com>
2025-04-21 17:53:31 +0530

    update upgrade page

--- src/app/upgrade/page.jsx
+++ src/app/upgrade/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -43,7 +43,7 @@ const plans = [
 
 const Page = () => {
   const { user } = useUser();
-  
+
   const router = useRouter();
   useEffect(() => {
     if (!user) {
